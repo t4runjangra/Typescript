d4ebeec6a0c13f9e1fd9f32c97d32c27b83ad7b8
t4runjangra <tarunjangra1903@gmail.com>
2026-07-19 21:53:38 +0530

    Add initial project setup with TypeScript configuration, package.json, and .gitignore

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+let username = "Tarun";
+
+username = "Rohit";
+
+const age = 12
+
+console.log(typeof age);
+
+type Direction = "left" | "right";
+
+type Status = "success" | "error";
+
+type DiceRoll = 1 | 2 | 3 | 4 | 5 | 6;
+
+const a = "hello";
+
+let b = "hello";
+
+let DiceRoll: DiceRoll = 2
+console.log(DiceRoll);
+
+
+let var1:any =10 
+var1 = 12 
+var1.toUpperCase()
+console.log(var1);
+
+//"any exists as an escape hatch. It allows developers to work with code when the type isn't known yet, when migrating JavaScript projects, or when interacting with untyped libraries. It trades type safety for flexibility.
